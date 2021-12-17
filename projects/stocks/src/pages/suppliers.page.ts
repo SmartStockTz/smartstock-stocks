b5bd79fa305ceb41706e3856a0cfb95826d24bdc
joshua <mama27j@gmail.com>
2021-12-17 16:44:48 +0100

    change supplier table view

--- projects/stocks/src/pages/suppliers.page.ts
+++ projects/stocks/src/pages/suppliers.page.ts
@@ -18,13 +18,7 @@ import {DeviceState} from '@smartstocktz/core-libs';
         <app-drawer></app-drawer>
       </ng-template>
       <ng-template #body>
-        <div
-          [class]="(deviceState.isSmallScreen | async)===true?'':'container col-lg-9 col-xl-9 col-sm-12 col-md-10 col-12 pt-3'"
-          style="min-height: 100vh">
-          <div style="margin: 4px 0">
-            <app-suppliers></app-suppliers>
-          </div>
-        </div>
+        <app-suppliers></app-suppliers>
       </ng-template>
     </app-layout-sidenav>
   `,
